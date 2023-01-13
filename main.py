3ab05879816d5a1e642e2c86aec381bb1b9ca60e
Dheeraj <dlovej009@gmail.com>
2023-01-13 10:52:18 +0530

    added_increase_rating_csv_structure

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def get_and_combine_all_data(url):
     twitter=[]
     instagram=[]
     email=[]
-    phone=[]
+    tiktok=[]
     response = requests.get(url,headers=head)
     soup = BeautifulSoup(response.text, 'html.parser')
     url_regex = r"((https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*))"
@@ -37,12 +37,17 @@ def get_and_combine_all_data(url):
             elif "instagram" in href:
                 if href not in instagram:
                     instagram.append(href)
+            elif "tiktok" in href:
+                if href not in tiktok:
+                    instagram.append(href)
     if facebook==[]:
         facebook.append("")
     if instagram==[]:
         instagram.append("")
     if twitter==[]:
         twitter.append("")
+    if tiktok==[]:
+        tiktok.append("")
     print("total urls found :- "+str(len(all_urls)))
     for i in all_urls:
         if i.startswith("/"):
@@ -66,59 +71,144 @@ def get_and_combine_all_data(url):
         "instagram":instagram[0],
         "facebook":facebook[0],
         "twitter":twitter[0],
+        "tiktok":tiktok[0],
         "email":email,
     }
     f=open("data.json","a",encoding="utf-8")
     f.write(str(df))
     return df  
 if __name__ == "__main__":
-    start_time=time.time()
-    # city_id= "85ab5e34-3d98-406f-a8c1-77df8ed68c2c"
-    # res=get_all_places(city_id)
+    names=[]
+    categories=[]
+    prize_ranges=[]
+    ratings=[]
+    review_n=[]
+    addresses=[]
+    Neighborhoods=[]
+    states=[]
+    cities=[]
+    countries=[]
+    zipcodes=[]
+    latitudes=[]
+    longitudes=[]
+    phones=[]
     URL=[]
     facebooks=[]
     twitters=[]
     instagrams=[]
     emails=[]
-    # try:
+    tiktoks=[]
     aa=Increas_rating()
-    all_dict=[]
-    # inr=0
     item_no=1
+    all_dict=[]
     total=len(aa["data"])
-    print(len(aa["data"]))
+    no=1
     for i in aa["data"]:
         try:
             print(f"Searching data for {item_no}/{total}")
-            d=urllib.parse.urlparse(str(i["FacebookLink"])) 
-            base_url = d.scheme + "://" +d.netloc
+            dD=urllib.parse.urlparse(str(i["FacebookLink"])) 
+            base_url = dD.scheme + "://" +dD.netloc
             print(base_url)
-            # url = 'http://bay6chennai.com/'
-            out=get_and_combine_all_data(base_url)
-            all_dict.append(out)
+            d=get_and_combine_all_data(base_url)
+            out={
+            "PlaceName":i['PlaceName'],
+            "PlaceType":i['PlaceType'],
+            "PriceRange":i['PriceRange'],
+            "Rating":i['Rating'],
+            "Rating_n":i['Rating_n'],
+            "Address":i['Address'],
+            "Neighborhood":i['Neighborhood'],
+            "states":"",
+            "cities":(i['Address']).split(",")[-2],
+            "Country":i['Country'],
+            "Latitude":i['Latitude'],
+            "Longitude":i['Longitude'],
+            "Zipcode":i['Zipcode'],
+            "PhoneNumber":i['PhoneNumber'],
+            "Business":d['Business'],
+            "instagram":d['instagram'],
+            "facebook":d['facebook'],
+            "twitter":d['twitter'],
+            "tiktok":d['tiktok'],
+            "emails":str(d['email']).replace("'","").replace("[","").replace("]","")}
             print(out)
-            print("")
-            print("")
-            print("")
-            print("")
-        except:
+            all_dict.append(out)
+                            
+        except Exception as err:
+            print(err)
             pass
         item_no+=1
-    # print(all_dict)
-    for d in all_dict:
-        URL.append(d['Business'])
-        instagrams.append(d['instagram'])
-        facebooks.append(d['facebook'])
-        twitters.append(d['twitter'])
-        emails.append(str(d['email']).replace("'","").replace("[","").replace("]",""))
-    
-    df=pd.DataFrame({
-    "Business":URL,
-    "instagram":instagrams,
-    "facebook":facebooks,
-    "twitter":twitters,
-    "emails":emails,
+        no+=1
+        if no>10:
+            break
+    print(len(all_dict))
+    for i in all_dict:
+            names.append(i['PlaceName'])
+            categories.append(i['PlaceType'])
+            prize_ranges.append(i['PriceRange'])
+            ratings.append(i['Rating'])
+            review_n.append(i['Rating_n'])
+            addresses.append(i['Address'])
+            Neighborhoods.append(i['Neighborhood'])
+            states.append("")
+            cities.append(i["cities"])
+            countries.append(i['Country'])
+            latitudes.append(i['Latitude'])
+            longitudes.append(i['Longitude'])
+            zipcodes.append(i['Zipcode'])
+            phones.append(i['PhoneNumber'])
+            URL.append(i['Business'])
+            instagrams.append(i['instagram'])
+            facebooks.append(i['facebook'])
+            twitters.append(i['twitter'])
+            tiktoks.append(i['tiktok'])
+            emails.append(i["emails"])
+          
+
+            
+            
+    print(len(names))     
+    print(len(categories))     
+    print(len(prize_ranges))     
+    print(len(review_n))     
+    print(len(addresses))     
+    print(len(Neighborhoods))     
+    print(len(cities))     
+    print(len(states))     
+    print(len(countries))     
+    print(len(zipcodes))     
+    print(len(latitudes))     
+    print(len(longitudes))     
+    print(len(URL))     
+    print(len(instagrams))     
+    print(len(facebooks))     
+    print(len(twitters))     
+    print(len(tiktoks))          
+    df=pd.DataFrame(
+    {
+    "Business Name": names,
+    "Category": categories,
+    "Price": prize_ranges,
+    "Rating": ratings,
+    "Review Count": review_n,
+    "Address": addresses,
+    "Neighborhood": Neighborhoods,
+    "City": cities,
+    "State": states,
+    "Country": countries,
+    "Zipcode": zipcodes,
+    "Latitude":latitudes,
+    "Longitude": longitudes,
+    "Phone Number": phones,
+    "Email": emails,
+    "Website":URL,
+    "Instagram":instagrams,
+    "Facebook": facebooks,
+    "Twitter": twitters,
+    "Tiktok": tiktoks
 })
-    df.to_csv("INcrease_Rating_contact_info.csv")
+    df.to_csv("INcrease_Rating_contact_info.csv",index=False)
   
+
+
 
